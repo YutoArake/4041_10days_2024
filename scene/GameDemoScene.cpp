7fbe323a2ec10d1cb1a12e28771735e106f0a3bb
YutoArake <94943725+YutoArake@users.noreply.github.com>
2024-09-09 11:34:17 +0900

    Stage読み込み機能

--- scene/GameDemoScene.cpp
+++ scene/GameDemoScene.cpp
@@ -3,7 +3,6 @@
 
 void GameDemoScene::Initialize()
 {
-	meteor.Initialize();
 }
 
 void GameDemoScene::Finalize()
@@ -12,8 +11,6 @@ void GameDemoScene::Finalize()
 
 void GameDemoScene::Update(char keys[256], char oldkeys[256])
 {
-	meteor.Update();
-
 	if (keys[KEY_INPUT_ESCAPE] == true &&
 		oldkeys[KEY_INPUT_ESCAPE] == false) {
 		SceneManager::GetInstance()->ChangeScene("TITLE");
@@ -24,5 +21,4 @@ void GameDemoScene::Update(char keys[256], char oldkeys[256])
 void GameDemoScene::Draw()
 {
 	DrawFormatString(0, 0, GetColor(255, 0, 0), "demo");
-	meteor.Draw();
 }
