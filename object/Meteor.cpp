5fa8fa468209851d41b71823315eaf092284fb84
YutoArake <94943725+YutoArake@users.noreply.github.com>
2024-09-11 15:07:26 +0900

    メテオの挙動追加

--- object/Meteor.cpp
+++ object/Meteor.cpp
@@ -9,11 +9,29 @@ void Meteor::Initialize(ObjectStatus status)
 	status_.R = status.R;
 	status_.Tag = status.Tag;
 
+	// 沸き場所
+	// 上
+	if (status_.Y < 0) {
+		// 左
+		if (status_.X < 32) pos = Posision::LeftTop;
+		// 右
+		else if (status_.X > 1248) pos = Posision::RightTop;
+	}
+	// 下
+	else if (status_.Y >= 1000) {
+		pos = Posision::Under;
+		// 左
+		if (status_.X < 32) pos = Posision::LeftUnder;
+		// 右
+		else if (status_.X > 1248) pos = Posision::RightUnder;
+	}
 	// タイマー
 	timer = 10;
 
 	// 画像データ
 	meteorGraph = LoadGraph("Resources/textures/meteor.png");
+	// 音声データ
+	seHandle = LoadSoundMem("Resources/sounds/se_meteor.mp3");
 }
 
 void Meteor::Finalize()
@@ -22,7 +40,30 @@ void Meteor::Finalize()
 
 void Meteor::Update()
 {
-	status_.Y -= 1.0f;
+	switch (pos)
+	{
+	case Meteor::Posision::LeftTop:
+		status_.Y += 3.0f;
+		status_.X += 3.0f;
+		break;
+	case Meteor::Posision::RightTop:
+		status_.Y += 3.0f;
+		status_.X -= 3.0f;
+		break;
+	case Meteor::Posision::Under:
+		status_.Y -= 1.0f;
+		break;
+	case Meteor::Posision::LeftUnder:
+		status_.Y -= 1.0f;
+		status_.X += 1.0f;
+		break;
+	case Meteor::Posision::RightUnder:
+		status_.Y -= 1.0f;
+		status_.X -= 1.0f;
+		break;
+	}
+
+	// タイマー
 	timer--;
 
 	// 全エフェクトの更新
@@ -32,7 +73,7 @@ void Meteor::Update()
 		ObjectStatus status;
 		status.X = status_.X;
 		status.Y = status_.Y;
-		status.R = 32;
+		status.R = status_.R;
 		// 宣言、生成
 		std::unique_ptr<FireEffect> effect;
 		effect = std::make_unique<FireEffect>();
@@ -54,5 +95,7 @@ void Meteor::Draw(float scroll)
 void Meteor::Collision()
 {
 	//当たったら消える
-	status_.X = -50;
+	status_.X = -5000;
+	// 効果音
+	PlaySoundMem(seHandle, DX_PLAYTYPE_BACK);
 }
